6b702fab203ccbc94b74414c2ae4e0945b4d1d1d
Prakrut Patel <49727154+prakrutpatel@users.noreply.github.com>
2021-01-18 23:00:30 -0500

    Initial commit

---
+++ lib/auth.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:flutter/services.dart';
+
+import 'home.dart';
+
+
+class AuthApp extends StatefulWidget {
+  @override
+  _AuthAppState createState() => _AuthAppState();
+}
+
+class _AuthAppState extends State<AuthApp> {
+  LocalAuthentication auth = LocalAuthentication();
+  bool _canCheckBiometric;
+  List<BiometricType> _availableBiometric;
+  String authorized = "Not authorized";
+
+  //checking bimetrics
+  //this function will check the sensors and will tell us
+  // if we can use them or not
+  Future<void> _checkBiometric() async{
+    bool canCheckBiometric;
+    try{
+      canCheckBiometric = await auth.canCheckBiometrics;
+    } on PlatformException catch(e){
+      print(e);
+    }
+    if(!mounted) return;
+
+    setState(() {
+      _canCheckBiometric = canCheckBiometric;
+    });
+  }
+
+  //this function will get all the available biometrics inside our device
+  //it will return a list of objects, but for our example it will only
+  //return the fingerprint biometric
+  Future<void> _getAvailableBiometrics() async{
+    List<BiometricType> availableBiometric;
+    try{
+      availableBiometric = await auth.getAvailableBiometrics();
+    } on PlatformException catch(e){
+      print(e);
+    }
+    if(!mounted) return;
+
+    setState(() {
+      _availableBiometric = availableBiometric;
+    });
+  }
+
+  //this function will open an authentication dialog
+  // and it will check if we are authenticated or not
+  // so we will add the major action here like moving to another activity
+  // or just display a text that will tell us that we are authenticated
+  Future<void> _authenticate() async{
+    bool authenticated = false;
+    try{
+      authenticated = await auth.authenticateWithBiometrics(
+          localizedReason: "Scan your finger print to authenticate",
+          useErrorDialogs: true,
+          stickyAuth: false
+      );
+    } on PlatformException catch(e){
+      print(e);
+    }
+    if(!mounted) return;
+
+    setState(() {
+      authorized = authenticated ? "Authorized" : "Failed to authenticate";
+    });
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _checkBiometric();
+    _getAvailableBiometrics();
+    _authenticate();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: <Widget>[
+            Center(
+            ),
+            Text("Can check biometric: $_canCheckBiometric"),
+            SizedBox(height: 50),
+            Text("Available biometric: "),
+            Text("$_availableBiometric"),
+            SizedBox(height: 50),
+            Text("Current State: $authorized"),
+          ],
+        ),
+      ),
+    );
+  }
+}
